fff4f93a33433dbab408b83f3fd67dffda50deae
donya <donyaa@metropolia.fi>
2026-03-03 10:02:53 +0200

    correcting logic errorrrrssss

--- State/src/main/java/Assignment/ExpertState.java
+++ State/src/main/java/Assignment/ExpertState.java
@@ -14,6 +14,10 @@ public class ExpertState extends LevelState {
         switch (choice) {
             case 1:
                 getGameCharacter().train();
+                if(getGameCharacter().getHealthPoints() < 0){
+                    System.out.println("Player's health is below 0, you will restart the game...");
+                    this.getGameCharacter().setState(new NoviceState(this.getGameCharacter()));
+                }
                 if(getGameCharacter().getExperiencePoints() >= 50) {
                     System.out.println("User has gained 50 XP, you will level up to Master level!");
                     this.getGameCharacter().setState(new MasterState(this.getGameCharacter()));
@@ -25,6 +29,10 @@ public class ExpertState extends LevelState {
                 break;
             case 3:
                 getGameCharacter().fight();
+                if(getGameCharacter().getHealthPoints() < 0){
+                    System.out.println("Player's health is below 0, you will restart the game...");
+                    this.getGameCharacter().setState(new NoviceState(this.getGameCharacter()));
+                }
                 if(getGameCharacter().getExperiencePoints() >= 50) {
                     System.out.println("User has gained 50 XP, you will level up to Master level!");
                     this.getGameCharacter().setState(new MasterState(this.getGameCharacter()));
